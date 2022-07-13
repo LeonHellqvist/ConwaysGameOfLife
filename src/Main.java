c85bca694ac45028ee2ce7b02e6ccd9ade7a947f
LeonHellqvist <leon.norlin@outlook.com>
2022-07-13 21:12:16 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -11,16 +11,23 @@ public class Main {
         System.out.println("Conways' Game Of Life");
         System.out.println("######################");
         Scanner input = new Scanner(System.in);
-        System.out.print("Choose grid size (eg 16x16): ");
+        System.out.print("Choose grid size (eg 25x40): ");
         String gridSize = input.nextLine();
-        System.out.print("Choose starting positions (eg 1,0;1,1;1,2): ");
+        System.out.print("Choose starting positions (eg 5,5;5,6;4,6;6,6 (x, y): ");
         String startingPositions = input.nextLine();
 
         Grid grid = new Grid(gridSize, startingPositions);
         while (true) {
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
             grid.printGrid();
             grid.nextGeneration();
-            input.nextLine();
+            try {
+                Thread.sleep(300);
+            }
+            catch(Exception e) {
+                System.out.println("Time.sleep exception");
+            }
         }
     }
 }
